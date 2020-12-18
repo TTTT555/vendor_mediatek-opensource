d7f94e4fa98237adf7a65ba22e53ccc19469a5fa
TTTT555 <albukerko@gmail.com>
2020-12-18 22:17:51 +0000

    mtk: aee: Make inits only for system or only for vendor variants.

---
+++ external/aee/config_external/aeev.mk
@@ -0,0 +1,2 @@
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/init.aee.customer.vendor.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.aee.rc:mtk
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/aee-config:$(TARGET_COPY_OUT_VENDOR)/etc/aee-config:mtk
